3c9f70fc595ac0de003fd3f32e0da477e830ccbf
Zhenya12345hdbd <128744046+Zhenya12345hdbd@users.noreply.github.com>
2026-07-02 19:50:56 +0300

    Ini

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -2,58 +2,37 @@ import { useParams } from "react-router-dom";
 import Button from "../components/header/button";
 import Servise from "../components/servise/servise";
 import Product_down from "../components/product_down/product_down";
-<<<<<<< HEAD
-import { menu_about } from "./about_data";
-import Material from "../components/material/material";
-
-let ertt 
-let menuItem
-
-function About(aspid) {
-  const { path } = useParams();
-      
-      const menuItem = menu_about.find((item) => item.path === String(path));
-      if (path == "About"){
-         ertt = <Servise/>
-         
-      } 
-=======
 import { menu_about } from "../components/all_component_top/about_data";
+import Material from "../components/material/material";
 import All_component_top from "../components/all_component_top/all_component_top";
 import About_main from "../components/about_,main/about_main";
 
+let menuItem
 let ertt = <Servise/>;
 let display = 'block'
 
 function About(aspid) {
   const { path } = useParams();
+      
       const menuItem = menu_about.find((item) => item.path === String(path));
-
+  
       if (path == "About"){
          ertt = <Servise/>
          display = 'block'
       }
->>>>>>> c4b73d66bb148f4592b5eef3c012268e054caee4
        if (path == "Services"){
            ertt = <Product_down/>;
            display = 'block'
-      } 
-<<<<<<< HEAD
-      console.log(menuItem)
-  return (
-    <div>
-      <Material
-      big = {menuItem.path}
-      
-      />
-        {ertt}
-=======
-       if (path == "Contact"){
-           ertt = <Product_down/>;
+      } if (path == "Contact"){           
+       ertt = <Product_down/>
             display = 'none'
->>>>>>> c4b73d66bb148f4592b5eef3c012268e054caee4
 
       } 
+     
+
+    
+        
+       
 
   return (
     <div> 
@@ -64,6 +43,7 @@ function About(aspid) {
         display = {display}
         />
         <About_main/>
+        
         
     </div>
   );
